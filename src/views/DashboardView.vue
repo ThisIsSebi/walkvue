57e2ab84519e1a3b7d9d26fd4fcdf7a40f0aadc6
ThisIsSebi <shoff439@gmail.com>
2025-04-22 06:42:17 +0200

    adjusted map to be displayed

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,10 +1,15 @@
 <script setup>
-import { usePoiStore, useCheckInStore, useAuthStore, useGeoStore } from "@/stores";
-import {onMounted, ref, watch} from "vue";
+import {
+  usePoiStore,
+  useCheckInStore,
+  useAuthStore,
+  useGeoStore,
+} from "@/stores";
+import { onMounted, ref, watch } from "vue";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine";
-import {usePictureStore} from "@/stores/picture.js";
+import { usePictureStore } from "@/stores/picture.js";
 import DashboardCarousel from "@/views/DashboardCarousel.vue";
 import { nextTick } from "vue";
 
@@ -17,50 +22,58 @@ const checkInStore = useCheckInStore();
 
 console.log(poiStore.poi);
 
-onMounted(() => {
+onMounted(async () => {
+  await nextTick();
   checkInStore.getCheckinsByUser();
-  nextTick(() => {
-    mapLoader();
-  });
+  mapLoader();
 });
 
-function mapLoader(){
-
+function mapLoader() {
   const mapElement = document.getElementById("map");
+  console.log("🧪 Map element:", mapElement);
+  console.log("🧪 mapElement offsetHeight:", mapElement?.offsetHeight);
   if (!mapElement) {
     console.warn("🛑 Map container not found!");
     return;
   }
 
-  if (map.value){
+  if (map.value) {
     map.value.remove();
     map.value = null;
   }
   map.value = L.map("map").setView([48.184606, 16.420382], 15);
 
   L.tileLayer(url, {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  subdomains: ['a', 'b', 'c']
-}).addTo(map);
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: ["a", "b", "c"],
+  }).addTo(map);
 
-setTimeout(() => {
+  setTimeout(() => {
     map.value.invalidateSize();
-  }, 100);
+  }, 300);
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-        (position) => {
-          userLocation.value.latitude = position.coords.latitude;
-          userLocation.value.longitude = position.coords.longitude;
-          map.value.setView([userLocation.value.latitude, userLocation.value.longitude], 15);
-          L.marker([userLocation.value.latitude, userLocation.value.longitude])
-              .addTo(map.value)
-              .bindPopup("Ihr Standort")
-              .openPopup();
-        },
-        (error) => {
-          console.error("Fehler beim Abrufen der Position:", error.code, error.message);
-        }
+      (position) => {
+        userLocation.value.latitude = position.coords.latitude;
+        userLocation.value.longitude = position.coords.longitude;
+        map.value.setView(
+          [userLocation.value.latitude, userLocation.value.longitude],
+          15
+        );
+        L.marker([userLocation.value.latitude, userLocation.value.longitude])
+          .addTo(map.value)
+          .bindPopup("Ihr Standort")
+          .openPopup();
+      },
+      (error) => {
+        console.error(
+          "Fehler beim Abrufen der Position:",
+          error.code,
+          error.message
+        );
+      }
     );
   } else {
     console.error("Geolocation wird nicht unterstützt");
@@ -87,9 +100,9 @@ setTimeout(() => {
 }
 
 watch(
-    () => checkInStore.checkins,
-    () => {
-      mapLoader(); 
+  () => checkInStore.checkins,
+  () => {
+    mapLoader();
 
     //    checkInStore.checkins.forEach((checkin) => {
     //   const poi = checkin.checkinPoi;
@@ -113,34 +126,32 @@ const fileInput = ref(null);
 const dialogVisible = ref(false);
 const checkinToEdit = ref();
 
-async function submitFileInput(){
+async function submitFileInput() {
   const formData = new FormData();
   formData.append("file", file.value);
-  console.log(formData)
+  console.log(formData);
   pictureStore.uploadPicture(formData, checkinToEdit.value.checkinPoi.poiId);
 
   //cancelWindow();
   //window.location.reload();
   console.log("Picturestore:" + pictureStore.pictures);
-
 }
 
-function openUploadDialog(checkin){
+function openUploadDialog(checkin) {
   checkinToEdit.value = checkin;
-  console.log(checkinToEdit.value)
+  console.log(checkinToEdit.value);
   dialogVisible.value = true;
 }
 
-function handleDelete(id){
-  checkInStore.deleteCheckin(id)
+function handleDelete(id) {
+  checkInStore.deleteCheckin(id);
   window.location.reload();
 }
 
-function cancelWindow(){
+function cancelWindow() {
   fileInput.value.value = null; // Setzt das Datei-Input zurück
-  dialogVisible.value = false;  // Schließt das Dialog
+  dialogVisible.value = false; // Schließt das Dialog
 }
-
 </script>
 
 <template>
@@ -150,14 +161,22 @@ function cancelWindow(){
     </v-row>
     <v-row justify="center">
       <v-col cols="12" md="8">
-        <div id="map" class="mapstyle" style="height: 500px; width: 100%; margin: 20px auto;"></div>
-
+        <div
+          id="map"
+          class="mapstyle"
+          style="height: 500px; width: 100%; margin: 20px auto"
+        ></div>
       </v-col>
     </v-row>
   </v-container>
   <v-container>
     <v-row>
-      <v-card elevation="1" class="v-col-12 v-col-md-6" id="POIDashboard" rounded="lg">
+      <v-card
+        elevation="1"
+        class="v-col-12 v-col-md-6"
+        id="POIDashboard"
+        rounded="lg"
+      >
         <h2>Meine besuchten Orte</h2>
         <!-- TEST - zu löschen:
           <table>
@@ -168,29 +187,41 @@ function cancelWindow(){
           </table>
         -->
 
-          <v-list activatable="activatable">
-            <v-list>
-              <v-list-item v-for="checkin in checkInStore.checkins"> <!-- ALT: v-for="(checkin, index) in checkInStore.checkins" :key="index"> -->
-                <v-row class="d-flex align-center">
-                  <v-col class="d-flex" style="flex-grow: 1; width: 100%;">
-                    <RouterLink :to="'/checkin/' + checkin.checkinPoi.poiId" class="checkInPOITitle">{{checkin.checkinPoi.poiTitle}}</RouterLink>
-                  </v-col>
-                  <v-col cols="auto" class="d-flex justify-start button-group">
-                    <v-btn color="primary" @click="handleDelete(checkin.checkinPoi.poiId)" class="mb-2 mb-md-0">
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                    <v-btn color="secondary"  @click="openUploadDialog(checkin)" class="ml-1 mb-2 mb-md-0">
-                      <v-icon>mdi-camera</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-list-item>
-            </v-list>
+        <v-list activatable="activatable">
+          <v-list>
+            <v-list-item v-for="checkin in checkInStore.checkins">
+              <!-- ALT: v-for="(checkin, index) in checkInStore.checkins" :key="index"> -->
+              <v-row class="d-flex align-center">
+                <v-col class="d-flex" style="flex-grow: 1; width: 100%">
+                  <RouterLink
+                    :to="'/checkin/' + checkin.checkinPoi.poiId"
+                    class="checkInPOITitle"
+                    >{{ checkin.checkinPoi.poiTitle }}</RouterLink
+                  >
+                </v-col>
+                <v-col cols="auto" class="d-flex justify-start button-group">
+                  <v-btn
+                    color="primary"
+                    @click="handleDelete(checkin.checkinPoi.poiId)"
+                    class="mb-2 mb-md-0"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                  <v-btn
+                    color="secondary"
+                    @click="openUploadDialog(checkin)"
+                    class="ml-1 mb-2 mb-md-0"
+                  >
+                    <v-icon>mdi-camera</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-list-item>
           </v-list>
+        </v-list>
       </v-card>
-      <DashboardCarousel/>
+      <DashboardCarousel />
     </v-row>
-
   </v-container>
   <v-dialog v-model="dialogVisible" max-width="500px">
     <v-card>
@@ -199,11 +230,11 @@ function cancelWindow(){
       </v-card-title>
       <v-card-text>
         <input
-            type="file"
-            ref="fileInput"
-            @change="onFileChanged($event)"
-            accept="image/*"
-            capture
+          type="file"
+          ref="fileInput"
+          @change="onFileChanged($event)"
+          accept="image/*"
+          capture
         />
       </v-card-text>
       <v-card-actions>
@@ -214,10 +245,16 @@ function cancelWindow(){
   </v-dialog>
 </template>
 
-
 <style scoped>
-@media (max-width: 600px){
-  .button-group{
+#map {
+  height: 500px !important;
+  min-height: 300px;
+  width: 100%;
+  display: block;
+}
+
+@media (max-width: 600px) {
+  .button-group {
     display: grid;
     grid-template-columns: 1fr;
     width: 100%;
@@ -229,7 +266,7 @@ function cancelWindow(){
   width: 100%;
 }
 
-.title h1{
+.title h1 {
   display: flex;
   justify-content: center;
   margin-left: auto;
@@ -238,11 +275,11 @@ function cancelWindow(){
   margin-top: 20px;
 }
 
-#POIDashboard h2{
+#POIDashboard h2 {
   margin-bottom: 20px;
 }
 
-.text-right{
+.text-right {
   display: flex;
   justify-content: flex-end;
 }
@@ -256,7 +293,7 @@ function cancelWindow(){
   display: none;
 }
 
-.checkInPOITitle{
+.checkInPOITitle {
   color: black;
   text-align: left; /* Sicherstellen, dass der Titel immer links ausgerichtet ist */
   white-space: nowrap; /* Verhindert, dass der Text umbricht, falls zu lang */
