b79f6e88893a70dd74f66b9403150c8a03af8ea5
ThisIsSebi <shoff439@gmail.com>
2025-05-01 08:19:40 +0200

    Changed map to map.setView

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -52,7 +52,7 @@ setTimeout(() => {
         (position) => {
           userLocation.value.latitude = position.coords.latitude;
           userLocation.value.longitude = position.coords.longitude;
-          map([userLocation.value.latitude, userLocation.value.longitude], 15);
+          map.setView([userLocation.value.latitude, userLocation.value.longitude], 15);
           L.marker([userLocation.value.latitude, userLocation.value.longitude])
               .addTo(map)
               .bindPopup("Ihr Standort")
